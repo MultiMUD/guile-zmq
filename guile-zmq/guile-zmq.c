eb88a155300863f139dd5a44a3bd8c2109d07f17
MultiMUD's Benden Breghnowsky <benden@multimud.net>
2015-05-13 12:19:54 +0200

    Compile fixes, updates, temporary stuff.
    * Disable automatic freeing of ZMQ sockets and contexts.
       * we shouldn't be closing those anyways because of the side-effects
       * we should only close them explicitly
    * add (zmq-wrap-context ptr) which wraps an existing, C-code initialized ZMQ context
    * move to msg variants of sending and receiving
    * change API to support handling multi-framed ZMQ messages
    * zmq-msg-send takes a vector of bytevectors now, each bv being a frame
    * zmq-msg-recv returns a vec of bvs now, each bv being a frame
    * zmq-poll added for sane integration of zmq with other socket/fd based event sources
    * clean up / update list of exported symbols

--- guile-zmq/guile-zmq.c
+++ guile-zmq/guile-zmq.c
@@ -67,12 +67,15 @@ static void* scm_to_zmq_context (SCM obj)
 
 static size_t scm_zmq_context_free (SCM obj)
 {
+	#if 0
+	/* -- MSW -- disabled -- we don't allow scheme to shutdown our context! */
   void *ctx = (void*)SCM_SMOB_DATA (obj);
   if (ctx)
     { fprintf (stderr, "terminating %p\n", ctx);
       SCM_SET_SMOB_DATA (obj, 0);
       zmq_term (ctx);
     }
+  #endif
   return 0;
 }
 
@@ -98,6 +101,8 @@ static void* scm_to_zmq_socket (SCM obj)
 
 static size_t scm_zmq_socket_free (SCM obj)
 {
+	#if 0
+	/* -- MSW -- disabled -- we don't allowe scheme to close our sockets implictly */
   void *sock = (void*)SCM_SMOB_DATA (obj);
 
   if (sock)
@@ -106,6 +111,7 @@ static size_t scm_zmq_socket_free (SCM obj)
       SCM_SET_SMOB_DATA (obj, 0);
     }
   
+  #endif
   return 0;
 }
 
@@ -135,6 +141,24 @@ SCM_DEFINE (scm_zmq_version, "zmq-version", 0, 0, 0,
                                  scm_from_int (patch)));
 }
 #undef FUNC_NAME
+
+SCM_DEFINE (scm_zmq_wrap_context, "zmq-wrap-context", 0, 1, 0,
+		(SCM ptr),
+		"Wrap a foreign pointer representing a ZMQ Context in a scheme variable for use with the library.")
+#define FUNC_NAME s_scm_zmq_wrap_context
+{
+	void *ctx;
+	if (SCM_UNBNDP(ptr))
+		scm_zmq_error(FUNC_NAME);
+
+	ctx = scm_to_pointer(ptr);
+	if (!ctx)
+		scm_zmq_error(FUNC_NAME);
+
+  return scm_from_zmq_context (ctx);
+}
+#undef FUNC_NAME
+
 
 
 SCM_DEFINE (scm_zmq_init, "zmq-init", 0, 1, 0,
@@ -314,52 +338,60 @@ SCM_DEFINE (scm_zmq_connect, "zmq-connect", 2, 0, 0,
 #undef FUNC_NAME
 
 
-SCM_DEFINE (scm_zmq_send, "zmq-send", 2, 1, 0,
-	    (SCM socket, SCM bv, SCM flags),
-	    "Send @var{bv} over @var{socket}.")
-#define FUNC_NAME s_scm_zmq_send
+SCM_DEFINE (scm_zmq_msg_send, "zmq-msg-send", 2, 1, 0,
+	    (SCM socket, SCM vec, SCM flags),
+	    "Send @var{vec}, a vector of bytevectors, as one frame per bv over @var{socket}.")
+#define FUNC_NAME s_scm_zmq_msg_send
 {
   void *s;
-  int cflags;
-  zmq_msg_t msg;
+  int cflags, i, len, curflag;
+  SCM bv;
   
   s = scm_to_zmq_socket (socket);
-  SCM_VALIDATE_BYTEVECTOR (2, bv);
+  SCM_VALIDATE_VECTOR (2, vec);
   if (SCM_UNBNDP (flags))
     cflags = 0;
   else
     cflags = scm_to_int (flags);
-  
-  if (zmq_msg_init_size (&msg, SCM_BYTEVECTOR_LENGTH (bv)))
-    scm_zmq_error (FUNC_NAME);
-  
-  memcpy (zmq_msg_data (&msg), SCM_BYTEVECTOR_CONTENTS (bv),
-          SCM_BYTEVECTOR_LENGTH (bv));
-  
-  if (zmq_send (s, &msg, cflags))
-    {
-      int errno_save = errno;
-      zmq_msg_close (&msg);
-      errno = errno_save;
-      scm_zmq_error (FUNC_NAME);
-    }
 
-  zmq_msg_close (&msg);
+  /* we're controlling setting of ZMQ_SNDMORE ourselves */
+  cflags &= ~ZMQ_SNDMORE;
+  
+  len = scm_to_int(scm_vector_length(vec));
+  for (i = 0; i < len; ++i) {
+	  bv = scm_vector_ref(vec, scm_from_int(i));
+	  zmq_msg_t msg;
+	  if (zmq_msg_init_size (&msg, SCM_BYTEVECTOR_LENGTH (bv)))
+		  scm_zmq_error (FUNC_NAME);
 
-  return SCM_UNSPECIFIED;
+	  memcpy (zmq_msg_data (&msg), SCM_BYTEVECTOR_CONTENTS (bv), 
+			  SCM_BYTEVECTOR_LENGTH (bv));
+
+	  curflag = cflags;
+	  if (i != len-1)
+		  curflag |= ZMQ_SNDMORE;
+	  if (zmq_msg_send (&msg, s, curflag) < 0) {
+		  int errno_save = errno;
+		  zmq_msg_close (&msg);
+		  errno = errno_save;
+		  scm_zmq_error (FUNC_NAME);
+	  } 
+	  zmq_msg_close (&msg);
+  }
+
+  return scm_from_int(len);
 }
 #undef FUNC_NAME
 
 
-SCM_DEFINE (scm_zmq_recv, "zmq-recv", 1, 1, 0,
+SCM_DEFINE (scm_zmq_msg_recv, "zmq-msg-recv", 1, 1, 0,
 	    (SCM socket, SCM flags),
 	    "Receive a message from @var{socket}.")
-#define FUNC_NAME s_scm_zmq_recv
+#define FUNC_NAME s_scm_zmq_msg_recv
 {
   void *s;
-  int cflags;
-  SCM ret;
-  zmq_msg_t msg;
+  int cflags, rc, thereismore=1;
+  SCM ret, msg_contents;
   
   s = scm_to_zmq_socket (socket);
   if (SCM_UNBNDP (flags))
@@ -367,19 +399,137 @@ SCM_DEFINE (scm_zmq_recv, "zmq-recv", 1, 1, 0,
   else
     cflags = scm_to_int (flags);
   
-  if (zmq_msg_init (&msg))
-    scm_zmq_error (FUNC_NAME);
-  
-  if (zmq_recv (s, &msg, cflags))
-    scm_zmq_error (FUNC_NAME);
+  ret = SCM_EOL;
+  do {
+	  zmq_msg_t msg;
+	  if (zmq_msg_init (&msg))
+		  scm_zmq_error (FUNC_NAME);
 
-  ret = scm_c_make_bytevector (zmq_msg_size (&msg));
-  memcpy (SCM_BYTEVECTOR_CONTENTS (ret), zmq_msg_data (&msg),
-          zmq_msg_size (&msg));
+	  rc=zmq_msg_recv (&msg, s, cflags);
+	  if (rc < 0 && !(cflags & ZMQ_DONTWAIT && errno == EAGAIN))
+		  scm_zmq_error (FUNC_NAME);
+	  else if (rc < 0 && (cflags & ZMQ_DONTWAIT && errno == EAGAIN))
+		  scm_throw(scm_from_locale_symbol("zmq-error"), scm_list_1(scm_from_locale_symbol("EAGAIN")));
 
-  zmq_msg_close (&msg);
+	  msg_contents = scm_c_make_bytevector (zmq_msg_size (&msg));
+	  memcpy (SCM_BYTEVECTOR_CONTENTS (msg_contents), zmq_msg_data (&msg),
+			  zmq_msg_size (&msg));
+	  ret = scm_cons(msg_contents, ret);
+	  thereismore = zmq_msg_more(&msg);
+	  zmq_msg_close (&msg);
+  } while (thereismore);
+
+  ret = scm_vector(scm_reverse(ret));
 
   return ret;
+}
+#undef FUNC_NAME
+
+SCM_DEFINE (scm_zmq_poll, "zmq-poll", 1, 1, 0,
+		(SCM pollitems, SCM timeout),
+		"Poll items in @var{pollitems}, waiting at most @var{timeout} ms (-1 = eternally, default = 0) for described activity.")
+#define FUNC_NAME s_scm_zmq_poll
+{
+	SCM pollitem, iter, pi_socket, pi_fdesc, pi_mode;
+	long t_o;
+	int i,nitems = 0, il, pi_type=0, pik = 0;
+	zmq_pollitem_t *pi = NULL;
+
+	if (SCM_UNBNDP(timeout)) 
+		t_o=0L;
+	else
+		t_o = scm_to_long(timeout);
+
+	if (!scm_is_pair(pollitems))
+		scm_zmq_error(FUNC_NAME);
+
+	nitems = scm_to_int(scm_length(pollitems));
+	if (!nitems)
+		scm_zmq_error(FUNC_NAME);
+
+	pi = calloc(nitems, sizeof(zmq_pollitem_t));
+
+	/* look for (socket? fdesc? mode) lists
+	 * either (list #f fdesc mode) or (list socket #f mode)
+	 */
+	for (i=0, iter=pollitems; i < nitems && scm_is_pair(iter) && !scm_is_null(iter); iter=scm_cdr(iter), ++i, ++pik) {
+		pollitem = scm_car(iter);
+		pi_type = 0;
+		if (!scm_is_pair(pollitem) || scm_is_null(pollitem))
+			scm_zmq_error(FUNC_NAME);
+		il=scm_to_int(scm_length(pollitem));
+		if (il != 3)
+			scm_zmq_error(FUNC_NAME);
+		pi_socket = scm_car(pollitem);
+		pi_fdesc = scm_cadr(pollitem);
+		pi_mode = scm_caddr(pollitem);	
+		if (scm_is_true(scm_boolean_p(pi_socket))) {
+			if (scm_is_true(pi_socket))
+				scm_zmq_error(FUNC_NAME);
+			else
+				pi_type = 1; /* fdesc */
+		}
+		if (scm_is_true(scm_boolean_p(pi_fdesc))) {
+			if (scm_is_true(pi_fdesc))
+				scm_zmq_error(FUNC_NAME);
+			else
+				pi_type = 2; /* socket */
+		}
+		switch (pi_type) {
+			case 1: /* listen on passed fdesc */
+				if (!scm_is_integer(pi_fdesc))
+					scm_zmq_error(FUNC_NAME);
+				pi[pik].fd = scm_to_int(pi_fdesc);
+				break;
+			case 2: /* listen on passed (wrapped) zmq socket */
+				if (!SCM_SMOB_PREDICATE(scm_tc16_zmq_socket, pi_socket))
+					scm_zmq_error(FUNC_NAME);
+				pi[pik].socket = scm_to_zmq_socket(pi_socket);
+				break;
+			default:
+				scm_zmq_error(FUNC_NAME);
+				break;
+		}
+		if (!scm_is_integer(pi_mode))
+			scm_zmq_error(FUNC_NAME);
+		pi[pik].events = scm_to_int(pi_mode);
+	}
+
+	il=0;
+	for (i=0; i < nitems; ++i) {
+		if (pi[i].fd > 0 || pi[i].socket != NULL) {
+			il=1;
+			break;
+		}
+	}
+	if (!il)
+		scm_zmq_error(FUNC_NAME);
+
+	i=zmq_poll(pi, nitems, t_o);
+
+	if (i < 0) {
+		scm_zmq_error(FUNC_NAME);
+	}
+	if (i == 0) {
+		free(pi);
+		return SCM_EOL;
+	}
+
+	pollitem = scm_make_list(scm_from_int(i), SCM_EOL);
+	pik=0;
+	for (il=0; il < nitems; ++il) {
+		if (pi[il].revents != 0) { /* something happened here */
+			pi_mode = scm_from_int(pi[il].revents);
+			pi_fdesc = (pi[il].fd  ? scm_from_int(pi[il].fd) : SCM_BOOL_F);
+			pi_socket = (pi[il].socket ? scm_from_zmq_socket(pi[il].socket) : SCM_BOOL_F);
+			iter = scm_list_3(pi_socket, pi_fdesc, pi_mode);
+			scm_list_set_x(pollitem, scm_from_int(pik), iter);
+			++pik;
+		}
+	}
+
+	free(pi);
+	return pollitem;
 }
 #undef FUNC_NAME
 
@@ -417,18 +567,13 @@ scm_init_zmq (void)
     scm_c_define ("ZMQ_PUSH", scm_from_int (ZMQ_PUSH));
     scm_c_define ("ZMQ_XPUB", scm_from_int (ZMQ_XPUB));
     scm_c_define ("ZMQ_XSUB", scm_from_int (ZMQ_XSUB));
-    scm_c_define ("ZMQ_UPSTREAM", scm_from_int (ZMQ_UPSTREAM));
-    scm_c_define ("ZMQ_DOWNSTREAM", scm_from_int (ZMQ_DOWNSTREAM));
 
-    scm_c_define ("ZMQ_HWM", scm_from_int (ZMQ_HWM));
-    scm_c_define ("ZMQ_SWAP", scm_from_int (ZMQ_SWAP));
     scm_c_define ("ZMQ_AFFINITY", scm_from_int (ZMQ_AFFINITY));
     scm_c_define ("ZMQ_IDENTITY", scm_from_int (ZMQ_IDENTITY));
     scm_c_define ("ZMQ_SUBSCRIBE", scm_from_int (ZMQ_SUBSCRIBE));
     scm_c_define ("ZMQ_UNSUBSCRIBE", scm_from_int (ZMQ_UNSUBSCRIBE));
     scm_c_define ("ZMQ_RATE", scm_from_int (ZMQ_RATE));
     scm_c_define ("ZMQ_RECOVERY_IVL", scm_from_int (ZMQ_RECOVERY_IVL));
-    scm_c_define ("ZMQ_MCAST_LOOP", scm_from_int (ZMQ_MCAST_LOOP));
     scm_c_define ("ZMQ_SNDBUF", scm_from_int (ZMQ_SNDBUF));
     scm_c_define ("ZMQ_RCVBUF", scm_from_int (ZMQ_RCVBUF));
     scm_c_define ("ZMQ_RCVMORE", scm_from_int (ZMQ_RCVMORE));
@@ -436,10 +581,14 @@ scm_init_zmq (void)
     scm_c_define ("ZMQ_EVENTS", scm_from_int (ZMQ_EVENTS));
     scm_c_define ("ZMQ_TYPE", scm_from_int (ZMQ_TYPE));
     scm_c_define ("ZMQ_LINGER", scm_from_int (ZMQ_LINGER));
-    scm_c_define ("ZMQ_RECONNECT_IVL", scm_from_int (ZMQ_RECONNECT_IVL));
     scm_c_define ("ZMQ_BACKLOG", scm_from_int (ZMQ_BACKLOG));
-    scm_c_define ("ZMQ_RECOVERY_IVL_MSEC", scm_from_int (ZMQ_RECOVERY_IVL_MSEC));
     scm_c_define ("ZMQ_RECONNECT_IVL_MAX", scm_from_int (ZMQ_RECONNECT_IVL_MAX));
+
+	scm_c_define ("ZMQ_POLLIN", scm_from_int(ZMQ_POLLIN));
+	scm_c_define ("ZMQ_POLLOUT", scm_from_int(ZMQ_POLLOUT));
+	scm_c_define ("ZMQ_POLLERR", scm_from_int(ZMQ_POLLERR));
+	scm_c_define ("ZMQ_DONTWAIT", scm_from_int(ZMQ_DONTWAIT));
+
     
     scm_c_define ("ZMQ_NOBLOCK", scm_from_int (ZMQ_NOBLOCK));
     scm_c_define ("ZMQ_SNDMORE", scm_from_int (ZMQ_SNDMORE));
